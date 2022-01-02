7187781d1225cc140b8a57c512a5b5d8b94ffbf5
jinsun <yks9013@naver.com>
2022-01-02 19:28:44 +0900

    기본세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+// * react basic import
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+
+// * to use redux
 import { ConnectedRouter } from 'connected-react-router'
+import { history } from './redux/store'
+
+import main from './page/main'
 
 function App() {
-    return <div className="App">오, 하이 데얼</div>
+    return (
+        <React.Fragment>
+            <ConnectedRouter history={history}>
+                <Switch>
+                    <Route path="/" exact component={main}></Route>
+                </Switch>
+            </ConnectedRouter>
+        </React.Fragment>
+    )
 }
 
 export default App
